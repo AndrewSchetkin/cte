d4a2e6fc85f401eaba6f2e4c22f88582e2cdd346
Андрей Щёткин <andrey.schetkin@gmail.com>
2022-06-30 09:11:17 +0300

    mainpage education photos

--- public/index.php
+++ public/index.php
@@ -179,28 +179,28 @@ $APPLICATION->SetTitle("ЦТО KAMI");
             <a class="cert-block__img" href="img/content/cert.jpg" data-fancybox="cert"><img src="<?=SITE_TEMPLATE_PATH?>/img/content/cert.jpg" alt="Сертификат"></a>
             <div class="cert-block__content">
                 <h2 class="cert-block__title"><span>
-                        <?$APPLICATION->IncludeComponent(
+                        <? $APPLICATION->IncludeComponent(
                             "bitrix:main.include",
                             "",
-                            Array(
+                            array(
                                 "AREA_FILE_SHOW" => "file",
                                 "AREA_FILE_SUFFIX" => "",
                                 "EDIT_TEMPLATE" => "",
-                                "PATH" => SITE_TEMPLATE_PATH.'/include/mainpage/cert_title.php'
+                                "PATH" => SITE_TEMPLATE_PATH . '/include/mainpage/cert_title.php'
                             )
-                        );?>
+                        ); ?>
                     </span></h2>
                 <div class="cert-block__text">
-                    <?$APPLICATION->IncludeComponent(
+                    <? $APPLICATION->IncludeComponent(
                         "bitrix:main.include",
                         "",
-                        Array(
+                        array(
                             "AREA_FILE_SHOW" => "file",
                             "AREA_FILE_SUFFIX" => "",
                             "EDIT_TEMPLATE" => "",
-                            "PATH" => SITE_TEMPLATE_PATH.'/include/mainpage/cert_text.php'
+                            "PATH" => SITE_TEMPLATE_PATH . '/include/mainpage/cert_text.php'
                         )
-                    );?>
+                    ); ?>
                 </div>
             </div>
         </div>
@@ -325,48 +325,71 @@ $APPLICATION->SetTitle("ЦТО KAMI");
     </section>
     <section class="front-block">
         <div class="container">
-            <div class="front-block__title">Фото с обучения</div>
-            <div class="front-block__content">
-                <div class="gallery gallery_fw">
-                    <div class="swiper js-gallery-auto-items">
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide">
-                                <a href="<?=SITE_TEMPLATE_PATH?>/img/content/teachers1.jpg" data-fancybox="front-gallery"><img class="swiper-lazy" data-src="<?=SITE_TEMPLATE_PATH?>/img/content/teachers1.jpg" alt="Имя">
-                                    <div class="swiper-lazy-preloader"></div>
-                                </a></div>
-                            <div class="swiper-slide">
-                                <a href="<?=SITE_TEMPLATE_PATH?>/img/content/gallery-1.jpg" data-fancybox="front-gallery"><img class="swiper-lazy" data-src="<?=SITE_TEMPLATE_PATH?>/img/content/gallery-1.jpg" alt="Имя">
-                                    <div class="swiper-lazy-preloader"></div>
-                                </a></div>
-                            <div class="swiper-slide">
-                                <a href="<?=SITE_TEMPLATE_PATH?>/img/content/gallery-2.jpg" data-fancybox="front-gallery"><img class="swiper-lazy" data-src="<?=SITE_TEMPLATE_PATH?>/img/content/gallery-2.jpg" alt="Имя">
-                                    <div class="swiper-lazy-preloader"></div>
-                                </a></div>
-                            <div class="swiper-slide">
-                                <a href="<?=SITE_TEMPLATE_PATH?>/img/content/gallery-3.jpg" data-fancybox="front-gallery"><img class="swiper-lazy" data-src="<?=SITE_TEMPLATE_PATH?>/img/content/gallery-3.jpg" alt="Имя">
-                                    <div class="swiper-lazy-preloader"></div>
-                                </a></div>
-                            <div class="swiper-slide">
-                                <a href="<?=SITE_TEMPLATE_PATH?>/img/content/gallery-1.jpg" data-fancybox="front-gallery"><img class="swiper-lazy" data-src="<?=SITE_TEMPLATE_PATH?>/img/content/gallery-1.jpg" alt="Имя">
-                                    <div class="swiper-lazy-preloader"></div>
-                                </a></div>
-                            <div class="swiper-slide">
-                                <a href="<?=SITE_TEMPLATE_PATH?>/img/content/gallery-2.jpg" data-fancybox="front-gallery"><img class="swiper-lazy" data-src="<?=SITE_TEMPLATE_PATH?>/img/content/gallery-2.jpg" alt="Имя">
-                                    <div class="swiper-lazy-preloader"></div>
-                                </a></div>
-                            <div class="swiper-slide">
-                                <a href="<?=SITE_TEMPLATE_PATH?>/img/content/gallery-3.jpg" data-fancybox="front-gallery"><img class="swiper-lazy" data-src="<?=SITE_TEMPLATE_PATH?>/img/content/gallery-3.jpg" alt="Имя">
-                                    <div class="swiper-lazy-preloader"></div>
-                                </a></div>
-                        </div>
-                        <div class="swiper-controls">
-                            <div class="swiper-button-prev"></div>
-                            <div class="swiper-pagination"></div>
-                            <div class="swiper-button-next"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <? $APPLICATION->IncludeComponent(
+                "bitrix:news.list",
+                "education_photos",
+                array(
+                    "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                    "ADD_SECTIONS_CHAIN" => "N",
+                    "AJAX_MODE" => "N",
+                    "AJAX_OPTION_ADDITIONAL" => "",
+                    "AJAX_OPTION_HISTORY" => "N",
+                    "AJAX_OPTION_JUMP" => "N",
+                    "AJAX_OPTION_STYLE" => "N",
+                    "CACHE_FILTER" => "N",
+                    "CACHE_GROUPS" => "Y",
+                    "CACHE_TIME" => "36000000",
+                    "CACHE_TYPE" => "A",
+                    "CHECK_DATES" => "N",
+                    "DETAIL_URL" => "",
+                    "DISPLAY_BOTTOM_PAGER" => "N",
+                    "DISPLAY_DATE" => "N",
+                    "DISPLAY_NAME" => "N",
+                    "DISPLAY_PICTURE" => "N",
+                    "DISPLAY_PREVIEW_TEXT" => "N",
+                    "DISPLAY_TOP_PAGER" => "N",
+                    "FIELD_CODE" => array(
+                        0 => "DETAIL_PICTURE",
+                        1 => "",
+                    ),
+                    "FILTER_NAME" => "",
+                    "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                    "IBLOCK_ID" => "2",
+                    "IBLOCK_TYPE" => "mainpage",
+                    "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                    "INCLUDE_SUBSECTIONS" => "N",
+                    "MESSAGE_404" => "",
+                    "NEWS_COUNT" => "20",
+                    "PAGER_BASE_LINK_ENABLE" => "N",
+                    "PAGER_DESC_NUMBERING" => "N",
+                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "",
+                    "PAGER_SHOW_ALL" => "N",
+                    "PAGER_SHOW_ALWAYS" => "N",
+                    "PAGER_TEMPLATE" => ".default",
+                    "PAGER_TITLE" => "",
+                    "PARENT_SECTION" => "",
+                    "PARENT_SECTION_CODE" => "",
+                    "PREVIEW_TRUNCATE_LEN" => "",
+                    "PROPERTY_CODE" => array(
+                        0 => "",
+                        1 => "",
+                    ),
+                    "SET_BROWSER_TITLE" => "N",
+                    "SET_LAST_MODIFIED" => "N",
+                    "SET_META_DESCRIPTION" => "N",
+                    "SET_META_KEYWORDS" => "N",
+                    "SET_STATUS_404" => "N",
+                    "SET_TITLE" => "N",
+                    "SHOW_404" => "N",
+                    "SORT_BY1" => "SORT",
+                    "SORT_BY2" => "",
+                    "SORT_ORDER1" => "ASC",
+                    "SORT_ORDER2" => "",
+                    "STRICT_SECTION_CHECK" => "N",
+                    "COMPONENT_TEMPLATE" => "education_photos"
+                ),
+                false
+            ); ?>
         </div>
     </section>
     <section class="front-block front-block_gray">
---
+++ public/local/templates/main/components/bitrix/news.list/education_photos/template.php
@@ -0,0 +1,39 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="front-block__title"><?=GetMessage("TITLE")?></div>
+<div class="front-block__content">
+    <div class="gallery gallery_fw">
+        <div class="swiper js-gallery-auto-items">
+            <div class="swiper-wrapper">
+                <? foreach ($arResult["ITEMS"] as $k => $arItem): ?>
+                    <?
+                    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                    ?>
+                    <div class="swiper-slide" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+                        <a href="<?=$arItem["DETAIL_PICTURE"]["SRC"]?>" data-fancybox="front-gallery"><img class="swiper-lazy" data-src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="<?=$arItem["NAME"];?>">
+                            <div class="swiper-lazy-preloader"></div>
+                        </a>
+                    </div>
+                <? endforeach; ?>
+            </div>
+            <div class="swiper-controls">
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-pagination"></div>
+                <div class="swiper-button-next"></div>
+            </div>
+        </div>
+    </div>
+</div>
