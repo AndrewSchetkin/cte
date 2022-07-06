5b25509645b788affea266c56823136915b8dfab
Андрей Щёткин <andrey.schetkin@gmail.com>
2022-07-06 12:37:49 +0300

    politics

--- public/local/templates/main/include/copyright_text.php
+++ public/local/templates/main/include/copyright_text.php
@@ -1,3 +1,3 @@
-<p>Вы принимаете условия <a href="#" target="_blank" rel="nofollow">политики конфиденциальности</a> и
-    <a href="#" target="_blank" rel="nofollow">пользовательского соглашения</a> каждый раз, когда
+<p>Вы принимаете условия <a href="/politika/" target="_blank" rel="nofollow">политики конфиденциальности</a> и
+    <a href="/polzovatelskoe/" target="_blank" rel="nofollow">пользовательского соглашения</a> каждый раз, когда
     оставляете свои данные в любой форме обратной связи на сайте stanki.ru.</p>
